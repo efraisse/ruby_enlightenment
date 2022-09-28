868071f08a9c4ee6ae6884ed5e3887930f4bcdf1
Erwan Fraisse <efraisse@ucsb.edu>
2022-09-28 14:24:04 -0700

    90 percent done

--- rubykoans/koans/about_dice_project.rb
+++ rubykoans/koans/about_dice_project.rb
@@ -6,6 +6,20 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  values = Array.new
+
+  def roll(num) 
+    temp = []
+    for i in 1..num do
+      temp.push(rand(1...7))
+    end
+    @values = temp
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
